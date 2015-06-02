22048648390a110929d70cc35a9875405e17b0f1
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-06-02 02:16:56 +0100

    Fix and improve the travis script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,15 @@
-echo "yes" | sudo add-apt-repository ppa:avsm/ocaml312+opam12
+case "$OCAML_VERSION" in
+    3.12) ppa=avsm/ocaml312+opam12;;
+    4.00) ppa=avsm/ocaml40+opam12;;
+    4.01) ppa=avsm/ocaml41+opam12;;
+    4.02) ppa=avsm/ocaml42+opam12;;
+    *) echo Unknown $OCAML_VERSION; exit 1;;
+esac
+
+sudo add-apt-repository --yes ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers opam
+sudo apt-get install -qq ocaml ocaml-native-compilers
+sh ./opam_installer.sh /usr/local/bin system # The ppa is not up-to-date
 export OPAMYES=1
 opam init
 eval `opam config env`
