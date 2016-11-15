1236526a9845d1c479bde952b4f43ec92a084b63
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2016-11-15 09:59:41 +0100

    Update travis tests

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,14 +1,6 @@
-case "$OCAML_VERSION" in
-    3.12) ppa=avsm/ocaml312+opam12;;
-    4.00) ppa=avsm/ocaml40+opam12;;
-    4.01) ppa=avsm/ocaml41+opam12;;
-    4.02) ppa=avsm/ocaml42+opam12;;
-    *) echo Unknown $OCAML_VERSION; exit 1;;
-esac
-
-sudo add-apt-repository --yes ppa:$ppa
+sudo add-apt-repository --yes ppa:avsm/ocaml42+opam12
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq opam
 export OPAMYES=1
 opam init
 eval `opam config env`
